d9cb85ada9adaec735894c022e620744b6d84117
Kybernetique <shadaev2001@icloud.com>
2023-03-05 18:59:23 +0400

    migration

---
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -0,0 +1,22 @@
+drop table if exists categories;
+drop table if exists products;
+
+create table categories (
+    id bigint not null auto_increment,
+    name varchar(255) not null,
+    description text not null,
+    image varchar(255),
+    primary key (id));
+
+create table products (
+    id bigint not null auto_increment,
+    name varchar(255) not null,
+    description text not null,
+    image varchar(255),
+    price integer not null,
+    category_id bigint,
+    primary key (id));
+
+alter table products
+    add constraint products_category_fk
+    foreign key (category_id) references categories (id);
