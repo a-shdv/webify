2d87e3a4e05de6ab1f7ed22bd7bbac7a3ba2fcd8
Kybernetique <shadaev2001@icloud.com>
2023-03-10 05:51:59 +0400

    bugs fix

--- src/main/java/com/example/webify/controllers/MainController.java
+++ src/main/java/com/example/webify/controllers/MainController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MainController {
@@ -36,16 +38,16 @@ public class MainController {
         return "redirect:/";
     }
 
-//    @PostMapping("/filter")
-//    public String filter(@RequestParam String filter, Model model) {
-//        List<Post> posts;
-//
-//        if (filter != null && !filter.isEmpty()) {
-//            posts = postService.getPostByHeader(filter);
-//        } else {
-//            posts = postService.getPosts();
-//        }
-//        model.addAttribute("posts", posts);
-//        return "index";
-//    }
+    @PostMapping("/filter")
+    public String filter(@RequestParam String filter, Model model) {
+        List<Post> posts;
+
+        if (filter != null && !filter.isEmpty()) {
+            posts = postService.getPostByHeader(filter);
+        } else {
+            posts = postService.getPosts();
+        }
+        model.addAttribute("posts", posts);
+        return "index";
+    }
 }
