c0a4e790e9864246ec331b9b0f3f189f14c13a7e
Kybernetique <shadaev2001@icloud.com>
2023-04-30 08:59:45 +0400

    Refactor pdf methods

--- src/main/java/com/shadaev/webify/service/UserService.java
+++ src/main/java/com/shadaev/webify/service/UserService.java
@@ -1,10 +1,14 @@
 package com.shadaev.webify.service;
 
-import com.lowagie.text.*;
-import com.lowagie.text.pdf.*;
-import com.shadaev.webify.entity.*;
+import com.lowagie.text.Font;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
+import com.shadaev.webify.entity.OrderProduct;
+import com.shadaev.webify.entity.User;
+import com.shadaev.webify.entity.UserRole;
 import com.shadaev.webify.repository.OrderProductRepository;
 import com.shadaev.webify.repository.UserRepository;
+import com.shadaev.webify.service.helpers.PdfGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,8 +17,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
 
@@ -59,83 +61,19 @@ public class UserService implements UserDetailsService {
     }
 
     public ByteArrayOutputStream generatePdf() throws Exception {
-        List<OrderProduct> orderProducts = orderProductRepository.findAll();
+        PdfGenerator pdfGenerator = new PdfGenerator();
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
-        Document document = new Document();
-        PdfWriter.getInstance(document, outputStream);
-
-        BaseFont baseFont = BaseFont.createFont("c:/windows/fonts/arial.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-        Font font = new Font(baseFont, 12, Font.BOLD);
-
-        Paragraph documentHeader = new Paragraph("Заказ-наряд", font);
-        documentHeader.setSpacingAfter(15);
-        documentHeader.setAlignment(Element.ALIGN_CENTER);
-
-        document.open();
-        document.add(documentHeader);
-
-        PdfPTable table = new PdfPTable(6);
-
-        PdfPCell orderIdHeaderCell = new PdfPCell(new Phrase("Номер заказа", font));
-        orderIdHeaderCell.setPadding(5);
-        orderIdHeaderCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        orderIdHeaderCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-
-        PdfPCell orderCreationDateHeaderCell = new PdfPCell(new Phrase("Дата заказа", font));
-        orderCreationDateHeaderCell.setPadding(5);
-        orderCreationDateHeaderCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        orderCreationDateHeaderCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-
-        PdfPCell orderNameHeaderCell = new PdfPCell(new Phrase("Название товара", font));
-        orderNameHeaderCell.setPadding(5);
-        orderNameHeaderCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        orderNameHeaderCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-
-        PdfPCell orderPriceHeaderCell = new PdfPCell(new Phrase("Цена", font));
-        orderPriceHeaderCell.setPadding(5);
-        orderPriceHeaderCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        orderPriceHeaderCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-
-        PdfPCell orderQuantityHeaderCell = new PdfPCell(new Phrase("Количество", font));
-        orderQuantityHeaderCell.setPadding(5);
-        orderQuantityHeaderCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        orderQuantityHeaderCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-
-        PdfPCell orderTotalPriceHeaderCell = new PdfPCell(new Phrase("Итого", font));
-        orderTotalPriceHeaderCell.setPadding(5);
-        orderTotalPriceHeaderCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        orderTotalPriceHeaderCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-
-        table.addCell(orderIdHeaderCell);
-        table.addCell(orderCreationDateHeaderCell);
-        table.addCell(orderNameHeaderCell);
-        table.addCell(orderPriceHeaderCell);
-        table.addCell(orderQuantityHeaderCell);
-        table.addCell(orderTotalPriceHeaderCell);
-
-        for (OrderProduct op : orderProducts) {
-            LocalDateTime orderCreationDate = LocalDateTime.parse(op.getOrder().getCreatedDate().toString(),
-                    DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS"));
-
-            String orderId = op.getOrder().getId().toString();
-            String formattedOrderCreationDate = orderCreationDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-            String orderName = op.getProduct().getName();
-            String orderPrice = String.valueOf(op.getProduct().getPrice());
-            String orderQuantity = String.valueOf(op.getQuantity());
-            String orderTotalPrice = String.valueOf(op.getPrice());
-
-            table.addCell(new PdfPCell(new Phrase(orderId))); // Номер заказа
-            table.addCell(new PdfPCell(new Phrase(formattedOrderCreationDate))); // Дата заказа
-            table.addCell(new PdfPCell(new Phrase(orderName))); // Название товара
-            table.addCell(new PdfPCell(new Phrase(orderPrice))); // Цена
-            table.addCell(new PdfPCell(new Phrase(orderQuantity))); // Количество
-            table.addCell(new PdfPCell(new Phrase(orderTotalPrice))); // Итого
-        }
-
-        document.add(table);
-        document.close();
-
+        List<OrderProduct> orderProducts = orderProductRepository.findAll();
+        Font font = pdfGenerator.createFont();
+        PdfPCell[] pdfPCells = {
+                new PdfPCell(new Phrase("Номер заказа", font)),
+                new PdfPCell(new Phrase("Дата заказа", font)),
+                new PdfPCell(new Phrase("Название товара", font)),
+                new PdfPCell(new Phrase("Цена", font)),
+                new PdfPCell(new Phrase("Количество", font)),
+                new PdfPCell(new Phrase("Итого", font))
+        };
+        pdfGenerator.generatePdf(outputStream, orderProducts, pdfPCells);
         return outputStream;
     }
 }
---
+++ src/main/java/com/shadaev/webify/service/helpers/PdfGenerator.java
@@ -0,0 +1,92 @@
+package com.shadaev.webify.service.helpers;
+
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.BaseFont;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import com.shadaev.webify.entity.OrderProduct;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class PdfGenerator {
+
+    public void generatePdf(ByteArrayOutputStream outputStream, List<OrderProduct> orderProducts, PdfPCell[] pdfPCells) throws Exception {
+        Font font = createFont();
+        Document document = createDocument(outputStream, font);
+
+        PdfPTable table = new PdfPTable(pdfPCells.length);
+        createTable(table, pdfPCells, orderProducts);
+
+        document.add(table);
+        document.close();
+    }
+
+    public Document createDocument(ByteArrayOutputStream outputStream, Font font) throws DocumentException {
+        Document document = new Document();
+        Paragraph documentHeader = createDocumentHeader(font);
+
+        PdfWriter.getInstance(document, outputStream);
+
+        document.open();
+        document.add(documentHeader);
+        return document;
+    }
+
+    private Paragraph createDocumentHeader(Font font) {
+        Paragraph documentHeader = new Paragraph("Заказ-наряд", font);
+        documentHeader.setSpacingAfter(15);
+        documentHeader.setAlignment(Element.ALIGN_CENTER);
+        return documentHeader;
+    }
+
+    public Font createFont() throws DocumentException, IOException {
+        BaseFont baseFont = BaseFont.createFont("c:/windows/fonts/arial.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+        return new Font(baseFont, 12, Font.BOLD);
+    }
+
+    private void setFontStyles(PdfPCell pdfPCell) {
+        pdfPCell.setPadding(5);
+        pdfPCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        pdfPCell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+    }
+
+    private void setFontStylesForPdfPCells(PdfPCell[] pdfPCells) {
+        for (PdfPCell pdfPCell : pdfPCells) {
+            setFontStyles(pdfPCell);
+        }
+    }
+
+    private void createTable(PdfPTable table, PdfPCell[] pdfPCells, java.util.List<OrderProduct> orderProducts) {
+        setFontStylesForPdfPCells(pdfPCells);
+        for (PdfPCell pdfPCell : pdfPCells) {
+            table.addCell(pdfPCell);
+        }
+        addOrderProductsToTable(table, orderProducts);
+    }
+
+    private void addOrderProductsToTable(PdfPTable table, List<OrderProduct> orderProducts) {
+        for (OrderProduct op : orderProducts) {
+            LocalDateTime orderCreationDate = LocalDateTime.parse(op.getOrder().getCreatedDate().toString(),
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS"));
+
+            String orderId = op.getOrder().getId().toString();
+            String formattedOrderCreationDate = orderCreationDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+            String orderName = op.getProduct().getName();
+            String orderPrice = String.valueOf(op.getProduct().getPrice());
+            String orderQuantity = String.valueOf(op.getQuantity());
+            String orderTotalPrice = String.valueOf(op.getPrice());
+
+            table.addCell(new PdfPCell(new Phrase(orderId))); // Номер заказа
+            table.addCell(new PdfPCell(new Phrase(formattedOrderCreationDate))); // Дата заказа
+            table.addCell(new PdfPCell(new Phrase(orderName))); // Название товара
+            table.addCell(new PdfPCell(new Phrase(orderPrice))); // Цена
+            table.addCell(new PdfPCell(new Phrase(orderQuantity))); // Количество
+            table.addCell(new PdfPCell(new Phrase(orderTotalPrice))); // Итого
+        }
+    }
+}
